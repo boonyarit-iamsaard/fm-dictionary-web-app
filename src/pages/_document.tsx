587ff769916846ce81a6d00657a8d071bb3d0011
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-03-06 22:50:38 +0700

    build: add custom colors and fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,16 @@ export default function Document() {
           content="This is a solution to the Dictionary web app challenge on Frontend Mentor."
         />
         <link rel="icon" type="image/png" href="/images/favicon-32x32.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&family=Inter:wght@400;700&family=Lora:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fm: {
+          primary: '#A445ED',
+          secondary: '#FF5252',
+          gray: {
+            50: '#F4F4F4',
+            100: '#E9E9E9',
+            500: '#757575',
+            600: '#3A3A3A',
+            700: '#2D2D2D',
+            800: '#1F1F1F',
+            900: '#050505',
+          },
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif],
+        mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
